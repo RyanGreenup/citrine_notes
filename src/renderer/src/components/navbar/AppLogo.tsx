8d1385fb55f59ff72fd80ceafa64b0e8e7cacbbd
Ryan G <exogenesis@protonmail.com>
2025-04-07 17:23:24 +1000

    refactor: Remove unnecessary AI comment from AppLogo.tsx

--- src/renderer/src/components/navbar/AppLogo.tsx
+++ src/renderer/src/components/navbar/AppLogo.tsx
@@ -4,7 +4,6 @@ import { onCleanup, onMount, createSignal } from 'solid-js'
 /**
  * This simply returns the date and time as it's convenient for users.
  */
-// Remove the at word AI!
 const getCurrentDateTime = (): string => {
   const now = new Date()
   const options: Intl.DateTimeFormatOptions = {
