a2373d15772122a772aa37b62a57e6db50f49c0c
Ryan G <exogenesis@protonmail.com>
2025-04-07 14:07:05 +1000

    feat: Add function to display current date and time in AppLogo

--- src/renderer/src/components/navbar/AppLogo.tsx
+++ src/renderer/src/components/navbar/AppLogo.tsx
@@ -4,6 +4,11 @@ import type { Component } from 'solid-js'
 /**
  * This simply returns the date and time as it's convenient for users.
  */
+const getCurrentDateTime = (): string => {
+  const now = new Date();
+  return now.toLocaleString();
+}
+
 export const AppLogo: Component = () => {
   // Color scheme
   const HEADING_TEXT = "text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white"
@@ -11,8 +16,7 @@ export const AppLogo: Component = () => {
   return (
     <a href="#" class="flex ml-2 md:mr-24">
       <span class={`self-center ${HEADING_TEXT}`}>
-      // Replace this with a call to a function that returnes datetime AI!
-       2025-04-07 12:34:56
+        {getCurrentDateTime()}
       </span>
     </a>
   )
