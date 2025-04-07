8fc9583ed060a80a0e71c9f6c5307e7bcd823d10
Ryan G <exogenesis@protonmail.com>
2025-04-07 14:07:05 +1000

    refactor: Update AppLogo to include datetime comment

--- src/renderer/src/components/navbar/AppLogo.tsx
+++ src/renderer/src/components/navbar/AppLogo.tsx
@@ -1,16 +1,32 @@
-import type { Component } from 'solid-js'
 
 import type { Component } from 'solid-js'
 
+/**
+ * This simply returns the date and time as it's convenient for users.
+ */
 export const AppLogo: Component = () => {
   // Color scheme
   const HEADING_TEXT = "text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white"
-  
+
   return (
     <a href="#" class="flex ml-2 md:mr-24">
       <span class={`self-center ${HEADING_TEXT}`}>
-        Note App - 2025-04-07 12:34:56
+      // Replace this with a call to a function that returnes datetime AI!
+       2025-04-07 12:34:56
       </span>
     </a>
   )
 }
+
+// export const AppLogo: Component = () => {
+//   // Color scheme
+//   const HEADING_TEXT = "text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white"
+//
+//   return (
+//     <a href="#" class="flex ml-2 md:mr-24">
+//       <span class={`self-center ${HEADING_TEXT}`}>
+//         Note App
+//       </span>
+//     </a>
+//   )
+// }
